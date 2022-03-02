59026d0108dc0c102ed648af8f2a664041ec946e
Otto Jones <ottosjonesjr@gmail.com>
2022-03-01 19:21:20 -0500

    added more to comment route

--- controllers/places.js
+++ controllers/places.js
@@ -95,11 +95,16 @@ router.post("/:id/comment", (req, res) => {
   console.log(req.body);
   db.Place.findById(req.params.id)
     .then((place) => {
-      db.Comment.create(req.body);
-      place.comments.push(comment.id);
-      place.save().then(() => {
-        res.redirect(`/places/${req.params.id}`);
-      });
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          res.render("error404");
+        });
     })
     .catch((err) => {
       res.render("error404");
